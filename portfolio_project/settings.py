636b4b57116eab14c254694768fe0951bce57915
farzana780 <farzanaece2k15@gmail.com>
2025-06-28 19:06:00 +0600

    remove staticfiles_build

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles', 'static'),
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR)
 MEDIA_URL = '/image/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
