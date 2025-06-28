5135d69060120ae183b228720aaeb94f4475c529
farzana780 <farzanaece2k15@gmail.com>
2025-06-28 18:51:03 +0600

    update build.sh file

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building the project..."
-python3.12.3 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.12.3 manage.py makemigrations --noinput
-python3.12.3 manage.py migrate --noinput
+python manage.py makemigrations
+python manage.py migrate 
 
 echo "Collect Static..."
-python3.12.3 manage.py collectstatic --noinput --clear
+python3.12.3 manage.py collectstatic
