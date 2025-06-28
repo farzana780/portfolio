5686640e9c370adef5fed2e21b6ad2db59420840
farzana780 <farzanaece2k15@gmail.com>
2025-06-28 18:53:29 +0600

    python3.9 add

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 echo "Building the project..."
 pip install -r requirements.txt
 
-echo "Make Migration..."
-python manage.py makemigrations
-python manage.py migrate 
+# echo "Make Migration..."
+# python manage.py makemigrations
+# python manage.py migrate 
 
 echo "Collect Static..."
-python3.12.3 manage.py collectstatic
+python3.9 manage.py collectstatic
